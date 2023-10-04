73e5d9f805fafa233fd6ab5717f948f4b4ce02e7
Pepito Mercurio <coranthin.etheve@ynov.com>
2023-10-04 16:48:34 +0200

    pay

--- src/Compotants/Header/index.jsx
+++ src/Compotants/Header/index.jsx
@@ -4,10 +4,10 @@ import "../../Style/scss/Componants/Header/style.scss"
 export default function Header() {
     return (
         <div className="Header">
-            <h2>LOGO</h2>
+            <a href="/"><h2>LOGO</h2></a>
             <div className="Log">
-                <p>Se connecter</p>
-                <p>S'inscrire</p>
+                <a href="/login"><p>Se connecter</p></a>
+                <a href="/sign-in"><p>S'inscrire</p></a>
             </div>
         </div>
     )
