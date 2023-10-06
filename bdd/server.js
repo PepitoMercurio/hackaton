eaec28beb64cd552007a8280f9032d99969594bd
Pepito Mercurio <coranthin.etheve@ynov.com>
2023-10-06 20:47:09 +0200

    login

--- bdd/server.js
+++ bdd/server.js
@@ -1,39 +1,32 @@
 const express = require('express');
-const mysql = require('mysql');
+const sqlite3 = require('sqlite3').verbose();  // Correction pour utiliser SQLite3 correctement
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3001;
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: null,
-  database: 'hackaton',
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+const db = new sqlite3.Database('./hackaton.db', (err) => {  // Correction de la création de la base de données SQLite
+  if (err)
+      throw err;
+
+  console.log('Database started on hackaton.db');  // Correction du message pour afficher le bon nom de la base de données
 });
 
-// Middleware pour analyser les données JSON
 app.use(bodyParser.json());
 
-// Connectez-vous à la base de données MySQL
-db.connect((err) => {
-  if (err) {
-    console.error('Erreur de connexion à la base de données : ' + err.message);
-    return;
-  }
-  console.log('Connecté à la base de données MySQL');
-});
-
-// Endpoint pour gérer l'inscription
 app.post('/sign-up', async (req, res) => {
   const { name, lastname, email, password } = req.body;
 
-  // Hachez le mot de passe avant de le stocker dans la base de données
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const query = 'INSERT INTO Users (name, lastname, email, password) VALUES (?, ?, ?, ?)';
-  db.query(query, [name, lastname, email, hashedPassword], (err, result) => {
+  db.run(query, [name, lastname, email, hashedPassword], (err) => {  // Utilisation de db.run() pour exécuter la requête SQLite
     if (err) {
       console.error('Erreur lors de l\'inscription : ' + err.message);
       res.status(500).json({ error: 'Erreur lors de l\'inscription' });
@@ -44,51 +37,49 @@ app.post('/sign-up', async (req, res) => {
   });
 });
 
-app.get('/test', (req, res) => {
-  const sqlQuery = 'SELECT * FROM users';
+app.get('/getUsers', (req, res) => {
+  const sqlQuery = 'SELECT * FROM Users';
 
-  // Exécutez la requête SQL
-  db.query(sqlQuery, (error, results) => {
+  db.all(sqlQuery, (error, results) => {  // Utilisation de db.all() pour récupérer tous les résultats
     if (error) {
       console.error('Erreur lors de l\'exécution de la requête SQL :', error);
       res.status(500).json({ error: 'Erreur lors de la récupération des données' });
       return;
     }
 
-    // Envoie les résultats en tant que réponse
     res.json(results);
   });
 });
 
-// Endpoint pour gérer la connexion
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+
+app.get('/check-login/:email/:password', async (req, res) => {
+  const { email, password } = req.params;
 
   // Recherchez l'utilisateur dans la base de données par email
-  db.query('SELECT * FROM Users WHERE email = ?', [email], async (err, results) => {
+  db.get('SELECT * FROM Users WHERE email = ?', [email], async (err, user) => {
     if (err) {
       console.error('Erreur lors de la recherche de l\'utilisateur : ' + err.message);
       res.status(500).json({ error: 'Erreur de serveur' });
     } else {
-      if (results.length === 0) {
+      if (!user) {
         // Aucun utilisateur trouvé avec cet email
-        res.status(401).json({ error: 'Utilisateur non trouvé' });
+        res.status(200).json({ result: 1 });  // Renvoie 1 si l'utilisateur n'est pas trouvé
       } else {
         // Comparez le mot de passe haché stocké avec le mot de passe fourni
-        const user = results[0];
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (passwordMatch) {
-          // Authentification réussie
-          res.status(200).json({ message: 'Connexion réussie' });
+          // Mot de passe correct, correspondance trouvée
+          res.status(200).json({ result: 0 });  // Renvoie 0 si l'utilisateur et le mot de passe correspondent
         } else {
           // Mot de passe incorrect
-          res.status(401).json({ error: 'Mot de passe incorrect' });
+          res.status(200).json({ result: 1 });  // Renvoie 1 si le mot de passe est incorrect
         }
       }
     }
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Serveur démarré sur le port ${port}`);
